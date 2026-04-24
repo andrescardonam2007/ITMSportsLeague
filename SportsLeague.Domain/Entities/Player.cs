6569a8e8d16bdec4b6e975fe8515ce52812b1f11
Andres Cardona Mejia <andrestwd158@gmail.com>
2026-04-24 16:58:18 -0500

    fase 5 finish

--- SportsLeague.Domain/Entities/Player.cs
+++ SportsLeague.Domain/Entities/Player.cs
@@ -13,5 +13,10 @@ namespace SportsLeague.Domain.Entities
         public int TeamId { get; set; }
         // Navigation Property
         public Team Team { get; set; } = null!;
+
+        // Agregar dentro de Player:
+        public ICollection<Goal> Goals { get; set; } = new List<Goal>();
+        public ICollection<Card> Cards { get; set; } = new List<Card>();
+
     }
 }
